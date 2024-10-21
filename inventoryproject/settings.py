5943845d0792f44af040cc2e8e98d17bcce8311b
Mohamed7422 <41089518+Mohamed7422@users.noreply.github.com>
2024-10-21 16:09:42 +0300

    updated my gitignore file2

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m1_$n13@8e4ma+sw2avip&xsx4gu_bbifcat@ljfxls25gs4e)'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
